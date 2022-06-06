fed6a85b2ce810717da464574fcd9e5bd8943c1b
BradyTaylor1996 <bgt8@duke.edu>
2022-06-06 11:04:10 -0400

    Added a python file

---
+++ inputStatResComp_v1.py
@@ -0,0 +1,12 @@
+# Input Stationary Feedforward Reservoir Computing 
+# vs. Standard Recurrent Reservoir Computing
+
+import warnings
+warnings.filterwarnings(action='ignore')
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+import sys
+import re
+np.set_printoptions(threshold=sys.maxsize)
